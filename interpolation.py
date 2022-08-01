e483ef36459443d9bf710b74a87733e62ab5ba09
Iskandar152 <issh0822@colorado.edu>
2022-07-31 18:43:45 -0600

    Multithreading polymatrix
    New file test_polymatrix_mult.py created, includes multithreading (minimal, only 2 threads, can be improved upon).

--- interpolation.py
+++ interpolation.py
@@ -3,9 +3,10 @@ Author: Iskandar Shoyusupov
 Date: 7/31/2022
 '''
 import numpy as np
+import time
 
-cmatrix_1 = np.matrix([1,2,3,4,5,6,7]) #Test coefficient matrix 1 
-cmatrix_2 = np.matrix([[7],[6],[5],[4],[3],[2],[1]])  #Test coefficient matrix 2 
+cmatrix_1 = np.matrix([1,2,3,4,5,6,7,8,9,10]) #Test coefficient matrix 1 
+cmatrix_2 = np.matrix([[10],[9],[8],[7],[6],[5],[4],[3],[2],[1]])  #Test coefficient matrix 2 
 def multiply(mat_1, mat_2):
     product = np.matmul(mat_1,mat_2)         #Product of the two coefficient matrices
     diagonal_loc = product.shape[0]          #Steps to diagonal
@@ -25,5 +26,9 @@ def multiply(mat_1, mat_2):
     print('-----------------')
     print("result coefficient matrix:")
     print(result)
-    
+
+start_time = time.time()
+
 multiply(cmatrix_2,cmatrix_1)
+
+print("Matrix mult took " , time.time() - start_time, "seconds")
---
+++ test_polymatrix_mult.py
@@ -0,0 +1,83 @@
+'''
+test_polymatrix_mult.py
+Author: Iskandar Shoyusupov
+Date: 7/31/2022
+Description: Testing time differences with multithreading and without
+             Looking if there is a significant difference
+
+             in progress
+'''
+
+import numpy as np
+import time
+import random
+import threading
+import os
+def random_c_matrix_creator(size):
+    rand_M = np.zeros(size, dtype = int) #Initializing matrix
+
+    for i in range(0,size):
+        rand_M[i] = random.randint(0,10)
+    return rand_M
+
+def no_thread_mult(mat_1,mat_2):
+    product = np.matmul(mat_1,mat_2)         #Product of the two coefficient matrices
+    diagonal_loc = product.shape[0]          #Steps to diagonal
+    dimension = (diagonal_loc * 2) -1        #Dimension of the vector space
+    result = np.zeros(dimension,dtype = int) #Result array
+    for i in range(0,diagonal_loc):
+        for j in range(0,i + 1):
+            result[i] += product[i - j,j]
+    for i in range(0,diagonal_loc - 1):
+        for j in range(0,diagonal_loc - i - 1):
+            result[diagonal_loc + i] += product[diagonal_loc - j - 1, j + 1 + i]
+
+    return result 
+
+
+def upper_calc(diagonal_loc,matrix,store):
+    dimension = (diagonal_loc * 2) -1
+    result = np.zeros(dimension,dtype = int)
+
+    for i in range(0,diagonal_loc):
+        for j in range(0,i + 1):
+            result[i] += matrix[i - j,j]
+    store.append(result)
+
+def lower_calc(diagonal_loc, matrix,store):
+    dimension = (diagonal_loc * 2) -1
+    result = np.zeros(dimension,dtype = int)
+    
+    for i in range(0,diagonal_loc - 1):
+        for j in range(0,diagonal_loc - i - 1):
+            result[diagonal_loc + i] += matrix[diagonal_loc - j - 1, j + 1 + i]
+    store.append(result)
+
+
+dimension = 10000
+
+a = random_c_matrix_creator(dimension).reshape(1,dimension)
+b = random_c_matrix_creator(dimension).reshape(dimension,1)
+c = np.matmul(b,a)
+
+print("MULTITHREADING___________________")
+start_time = time.time()
+
+store = []
+t1 = threading.Thread(target=upper_calc,args=(dimension,c, store), name="t1")
+t2 = threading.Thread(target=lower_calc,args=(dimension,c, store), name="t2")
+t1.start()
+t2.start()
+t1.join()
+t2.join()
+result = store[0] + store[1]
+print("Matrix mult took " , time.time() - start_time, "seconds")
+print(result)
+
+
+print("NON MULTI THREADING__________________________")
+start_time2 = time.time()
+non_thread_reuslt = no_thread_mult(b,a)
+print("Matrix mult took " , time.time() - start_time, "seconds")
+
+
